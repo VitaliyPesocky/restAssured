e6c4a931d61444f8d3f5fe967104c6b56aacbc70
VitaliyPesocky <vitaliypesocky@gmail.com>
2020-05-02 19:16:50 +0300

    trying use specifications

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -6,8 +6,12 @@ import static io.restassured.RestAssured.*;
 public class FirstTest extends TestConfig {
     @Test
     public void firstTest() {
-        given().header("Accept", "application/json").
+        given().
+                log().
+                all().
         when().get("videogames/1").
-        then().statusCode(200);
+        then().
+                log().
+                all();
     }
 }
